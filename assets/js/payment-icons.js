96e6a888b7166ea5a7077a3f73331834a55a6789
jarellano <jahn.arellano@agileengine.com>
2023-04-24 18:02:40 -0300

    add payment method icons to footer

---
+++ assets/js/payment-icons.js
@@ -0,0 +1,26 @@
+(function(){
+	var dir = "assets/payment-methods-icons/";
+    var fileextension = ".png";
+    var imgTags = '';
+    const images = [
+        'finance_payment_paypal_icon',
+        'card_payment_unionpay_icon',
+        'paypal_method_payment_icon',
+        'master_method_card_payment_icon',
+        'visa_method_card_payment_icon',
+        'american express_amex_billing_credit card_payment_icon',
+        'apple_logo_logos_pay_icon',
+        'card_club_diners_dinner_payment_icon',
+        'logo_pay_google_gpay_icon',
+        'card_money_payment_payu_icon'
+      ];
+      console.log('---');
+
+        images.forEach(url => {
+            var imgTag = '<img src="http://' +window.location.host+ '/'+dir + url + fileextension + '" alt>';
+            imgTags += imgTag;
+            console.log('image : '+imgTag);
+        });
+	
+    document.getElementById('payment-icons').innerHTML = imgTags;
+})();
